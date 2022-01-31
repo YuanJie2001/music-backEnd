662979856bdfcc5b692ac13e04b3e3952868e122
yuanjiejiahui <782353676@qq.com>
2022-01-31 19:10:53 +0800

    commit

---
+++ src/pages/Login.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="login-wrap">
+    <div class="ms-title">music 后台管理登录</div>
+    <div class="ms-login">
+      <!--      :model表单数据的存放名称  :rules提示输入信息的存放名称 ref使用该form的名称-->
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+        <el-form-item prop="username">
+          <el-input v-model="ruleForm.username" placeholder="用户名"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input type="password" v-model="ruleForm.password" placeholder="密码"></el-input>
+        </el-form-item>
+        <div class="login-btn">
+          <el-button type="primary" @click="submitForm">登录</el-button>
+        </div>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      ruleForm: {
+        username: 'admin',
+        password: '123'
+      },
+      rules: {
+        username: [
+          {required: true, message: '请输入用户名', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, message: '请输入密码', trigger: 'blur'}
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm () {
+      alert('提交')
+    }
+  }
+}
+</script>
+<style scoped>
+.login-wrap {
+  position: absolute;
+  background: url("../assets/img/backstage-bg.jpg");
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+}
+
+.ms-title {
+  position: relative;
+  top: 50%;
+  width: 100%;
+  margin-top: -180px;
+  text-align: center;
+  font-size: 30px;
+  font-weight: 600;
+  color: black;
+}
+
+.ms-login {
+  position: relative;
+  width: 300px;
+  height: 160px;
+  margin: 25% auto;
+  padding: 40px;
+  border-radius: 5px;
+  background: rgb(0,0,0,0.4);
+}
+
+.login-btn {
+  text-align: center;
+}
+
+.login-btn button {
+  width: 100%;
+  height: 36px;
+}
+</style>
